2a070f7bee1787d010d6a12998ac0a84a5b8893d
Umarbek-commits <sidikovoatillo44@gmail.com>
2026-03-12 12:47:51 +0600

    fix vercel routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
-import "./i18n";
-import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/inventory">
+    <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
